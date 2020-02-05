1b99042f595ac0deb95a3b48af244100d7a3d461
Johann Gouin <johann@infomaniak.com>
2020-02-05 08:06:36 +0100

    Allow QtWebEngineProcess to run JS code (due to change in sign option)

--- admin/osx/sign_app.sh
+++ admin/osx/sign_app.sh
@@ -7,11 +7,12 @@ identity="$2"
 team_identifier="$3"
 
 codesign -s "$identity" --force --preserve-metadata=entitlements --verbose=4 --deep --options=runtime "$src_app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
+codesign -s "$identity" --force --verbose=4 --deep --options=runtime --entitlements $(dirname $0)/QtWebEngineProcess.entitlements "$src_app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess"
 codesign -s "$identity" --force --preserve-metadata=entitlements --verbose=4 --deep --options=runtime "$src_app"
 
 # Verify the signature
 codesign -dv $src_app
-codesign --verify -v $src_app
+codesign --verify -v --strict $src_app
 # spctl -a -t exec -vv $src_app
 
 # Validate that the key used for signing the binary matches the expected TeamIdentifier
