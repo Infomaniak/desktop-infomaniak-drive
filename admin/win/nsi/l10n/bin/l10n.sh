36744b5cc55ebad89f1e5712cee228e3d4163bf0
unknown <christophe.larchier@infomaniak.com>
2020-07-23 16:30:05 +0200

    Windows: Fix installer issues

--- admin/win/nsi/l10n/bin/l10n.sh
+++ admin/win/nsi/l10n/bin/l10n.sh
@@ -12,28 +12,11 @@ cp -f $PODIR/messages.pot $PODIR/en.po
 # generate all the languages nsh files
 python $SCRIPTDIR/build_locale_nsi.py -o $L10NDIR -p $PODIR -l "English"
 
-# for future references: the windows code pages
-# 874 — Thai
-# 932 — Japanese
-# 936 — Chinese (simplified) (PRC, Singapore)
-# 949 — Korean
-# 950 — Chinese (traditional) (Taiwan, Hong Kong)
-# 1200 — Unicode (BMP of ISO 10646, UTF-16LE)
-# 1201 — Unicode (BMP of ISO 10646, UTF-16BE)
-# 1250 — Latin (Central European languages)
-# 1251 — Cyrillic
-# 1252 — Latin (Western European languages, replacing Code page 850)
-# 1253 — Greek
-# 1254 — Turkish
-# 1255 — Hebrew
-# 1256 — Arabic
-# 1257 — Latin (Baltic languages)
-# 1258 — Vietnamese
-
 # convert file to proper content
-#cd $L10NDIR
-#iconv -c -t CP1252 German.nsh > German2.nsh
-#iconv -c -t CP1252 English.nsh > English2.nsh
-#iconv -c -t CP1252 Italian.nsh > Italian2.nsh
-#iconv -c -t CP1252 Spanish.nsh > Spanish2.nsh
+cd $L10NDIR
+iconv -t CP1252 French.nsh > French2.nsh; rm French.nsh; mv French2.nsh French.nsh
+iconv -t CP1252 German.nsh > German2.nsh; rm German.nsh; mv German2.nsh German.nsh
+iconv -t CP1252 English.nsh > English2.nsh; rm English.nsh; mv English2.nsh English.nsh
+iconv -t CP1252 Italian.nsh > Italian2.nsh; rm Italian.nsh; mv Italian2.nsh Italian.nsh
+iconv -t CP1252 Spanish.nsh > Spanish2.nsh; rm Spanish.nsh; mv Spanish2.nsh Spanish.nsh
 
