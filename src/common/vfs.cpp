5368249be20c391703b71f3891d841758fd3164f
unknown <christophe.larchier@infomaniak.com>
2020-10-01 09:56:32 +0200

    wincfapi plugin

--- src/common/vfs.cpp
+++ src/common/vfs.cpp
@@ -118,7 +118,7 @@ static QString modeToPluginName(Vfs::Mode mode)
     if (mode == Vfs::WithSuffix)
         return "suffix";
     if (mode == Vfs::WindowsCfApi)
-        return "CloudFileProvider";
+        return "win";
     return QString();
 }
 
