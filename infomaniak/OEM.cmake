24e76c96c46846866f71c3cda1368f59a4e81096
Christophe Larchier <christophe.larchier@infomaniak.com>
2020-12-17 16:00:25 +0100

    Update ext packaging

--- infomaniak/OEM.cmake
+++ infomaniak/OEM.cmake
@@ -36,5 +36,6 @@ set( CRASHREPORTER_ICON ":/infomaniak.png" )
 set( DEBUGREPORTER_SUBMIT_URL "https://www.infomaniak.com/report/drive/logs" CACHE STRING "URL for debug reporter" )
 
 set( CLOUDFILEPROVIDER_DIRECTORY "C:\\Projects\\CloudFileProvider\\x64\\Release" )
-set( CLOUDFILEPROVIDER_APPX_DIRECTORY "C:\\Projects\\CloudFileProvider\\CloudFileProviderPackage\\bin\\x64\\Release\\AppX" )
-set( CLOUDFILEPROVIDER_APPX_PACKAGE "Infomaniak.kDrive.Extension_1.0.0.0_x64__vx2c2mhs9xqee" )
+#set( CLOUDFILEPROVIDER_APPX_DIRECTORY "C:\\Projects\\CloudFileProvider\\CloudFileProviderPackage\\bin\\x64\\Release\\AppX" )
+set( CLOUDFILEPROVIDER_APPX_DIRECTORY "C:\\Projects\\CloudFileProvider\\CloudFileProviderPackage\\AppPackages\\CloudFileProviderPackage" )
+set( CLOUDFILEPROVIDER_APPX_PACKAGE "Infomaniak.kDrive.Extension_1.0.0.0_x64__e4e5bp7s792f4" )
