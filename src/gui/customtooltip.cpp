fc8bf97fa6826b56044100f438278bd4f0147e70
Christophe Larchier <christophe.larchier@infomaniak.com>
2021-04-28 22:35:51 +0200

    Qt 5.15.2 migration

--- src/gui/customtooltip.cpp
+++ src/gui/customtooltip.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <QLabel>
 #include <QLoggingCategory>
 #include <QPainter>
+#include <QPainterPath>
 #include <QStyleOption>
 #include <QTimer>
 
--- src/gui/preferencesblocwidget.cpp
+++ src/gui/preferencesblocwidget.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <QGraphicsDropShadowEffect>
 #include <QLoggingCategory>
 #include <QPainter>
+#include <QPainterPath>
 #include <QScrollBar>
 
 namespace KDC {
